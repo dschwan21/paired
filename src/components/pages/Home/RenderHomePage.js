627ec16fe61ae95b77ccf87a459a919bf46493dc
Derek Schwantner <derek@Dereks-Mac-mini.local>
2022-03-25 01:55:21 -0400

    aligning test with text from homepage, again, again

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -6,7 +6,7 @@ function RenderHomePage(props) {
   const { userInfo, authService } = props;
   return (
     <div>
-      <h1>Hi {userInfo.name} welcome to Paired!</h1>
+      <h1>hi {userInfo.name} welcome to Paired!</h1>
       <div>
         <p>
           This is an example of a common example of how we'd like for you to
