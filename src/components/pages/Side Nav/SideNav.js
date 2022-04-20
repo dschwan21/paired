a02047f32df95b3665abb09fa60a1fad51320723
Derek Schwantner <derek@Dereks-Mac-mini.local>
2022-04-20 01:06:42 -0400

    added side nav component

---
+++ src/components/pages/Side Nav/SideNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Here we will create a react component that will create a side navigation bar with 6 links that navigate to the other pages. The nav will have the users avatar and name at the top. The navigation will also have these links: Home, Discover, Notifications, Newsfeed, Create, Saved.
+// The nav will also have a logout button that will log the user out of the application.
+
+const SideNav = () => {
+  return (
+    <div>
+      <h1>Side Nav</h1>
+    </div>
+  );
+};
+
+export default SideNav;
